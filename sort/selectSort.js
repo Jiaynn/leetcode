3a5c71417ccc253e1efd6b0033ea19e2d2d2bbee
Jiaynn <jiayan.li@js.design>
2025-02-22 14:13:35 +0800

    feat: 新增选择排序

--- sort/selectSort.js
+++ sort/selectSort.js
@@ -1,3 +1,19 @@
 function selectSort(arr) {
-  console.log(11);
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+    }
+    swap(arr, i, minIndex);
+  }
+  return arr;
 }
+
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+const arr = [3, 2, 5, 6, 3, 7, 2];
+console.log(selectSort(arr));
